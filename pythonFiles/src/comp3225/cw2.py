b7c34ae3203d36b249ecb51715b9099298565f8a
mishra1604 <aaryan.mishra.1604@gmail.com>
2023-03-15 13:19:58 +0000

    first iteration of regex

--- pythonFiles/src/comp3225/cw2.py
+++ pythonFiles/src/comp3225/cw2.py
@@ -8,13 +8,12 @@ import nltk, numpy, scipy, sklearn, sklearn_crfsuite, sklearn_crfsuite.metrics
 LOG_FORMAT = ('%(levelname) -s %(asctime)s %(message)s')
 logger = logging.getLogger( __name__ )
 logging.basicConfig( level=logging.INFO, format=LOG_FORMAT )
-logger.info('logging started')
 
 def test_question_regex():
     fname="../../corpus/comp3225/eval_chapter.txt"
 
     # question regex for a chapter in a book
-    question = r"(?:\.|!|\?)\s+(.*\?)\s+"
+    question = r"(?:\.|\?|^)\s?(.?[\w+\s]+\?.?\s)" #(?:\.|\?)\s+(.*\?)\s+ 
 
     matches = re.findall(question, codecs.open(fname,"r",encoding="utf-8").read(), re.MULTILINE)
     for match in matches:
