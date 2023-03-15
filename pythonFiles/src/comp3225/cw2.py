e622dcd29291b5cd10d831c98307047e4dae7ad8
mishra1604 <aaryan.mishra.1604@gmail.com>
2023-03-15 12:02:36 +0000

    Second cw, regex for chapter extraction

---
+++ pythonFiles/src/comp3225/cw2.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import sys, codecs, json, math, time, warnings, re, logging
+warnings.simplefilter( action='ignore', category=FutureWarning )
+
+import nltk, numpy, scipy, sklearn, sklearn_crfsuite, sklearn_crfsuite.metrics
+
+LOG_FORMAT = ('%(levelname) -s %(asctime)s %(message)s')
+logger = logging.getLogger( __name__ )
+logging.basicConfig( level=logging.INFO, format=LOG_FORMAT )
+logger.info('logging started')
+
+def test_question_regex():
+    fname="../../corpus/comp3225/eval_chapter.txt"
+
+    # question regex for a chapter in a book
+    question = r"(?:\.|!|\?)\s+(.*\?)\s+"
+
+    matches = re.findall(question, codecs.open(fname,"r",encoding="utf-8").read(), re.MULTILINE)
+    for match in matches:
+        print(match)
+
+if __name__ == '__main__':
+    test_question_regex()
